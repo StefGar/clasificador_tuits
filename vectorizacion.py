9ca9757e1ab8a25c46d0317d44e6c8b3e99a3543
Stefany García <faangaar@gmail.com>
2024-12-03 10:49:28 -0600

    Impresión de vocabulario generado

--- vectorizacion.py
+++ vectorizacion.py
@@ -3,7 +3,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Función para vectorizar texto utilizando TF-IDF
 def vectorizar_texto(textos):
     # Crear una instancia del vectorizador TF-IDF
-    vectorizer = TfidfVectorizer(max_features=1000)
+    vectorizer = TfidfVectorizer(max_features=5000)
+    #Impresión de vocabulario genrado por TfidfVectorizer
+    print(vectorizer.vocabulary_)
     # Ajustar y transformar los textos en una matriz TF-IDF
     X = vectorizer.fit_transform(textos)
     # Devolver la matriz TF-IDF y el vectorizador ajustado
