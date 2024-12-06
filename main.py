9ef95398f0f7327e56129dae16aafac543df56e8
Stefany García <faangaar@gmail.com>
2024-12-06 11:45:18 -0600

    test

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ tweets = []
 temas = []
 
 for query in queries:
-    t = obtener_tweets(query, count=500)  # Obtener 500 tweets por categoría
+    t = obtener_tweets(query, count=1000)  # Obtener 1000 tweets por categoría
     if not t:
         print(f"No tweets found for query: {query}")
         continue
--- modelo.py
+++ modelo.py
@@ -1,14 +1,29 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 
 # Función para entrenar el modelo
 def entrenar_modelo(X_train, y_train):
     # Crear una instancia del clasificador Random Forest
-    modelo = RandomForestClassifier(n_estimators=100, class_weight='balanced', random_state=42)
-    # Ajustar el modelo con los datos de entrenamiento
-    modelo.fit(X_train, y_train)
+    modelo = RandomForestClassifier(class_weight='balanced', random_state=42)
+    
+    # Definir los parámetros para GridSearchCV
+    param_grid = {
+        'n_estimators': [100, 200, 300],
+        'max_depth': [None, 10, 20, 30],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+    
+    # Realizar la búsqueda de hiperparámetros
+    grid_search = GridSearchCV(estimator=modelo, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+    grid_search.fit(X_train, y_train)
+    
+    # Obtener el mejor modelo
+    best_model = grid_search.best_estimator_
+    
     # Devolver el modelo entrenado
-    return modelo
+    return best_model
 
 # Función para evaluar el modelo
 def evaluar_modelo(modelo, X_test, y_test):
